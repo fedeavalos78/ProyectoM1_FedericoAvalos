678c6195827e0e6c0b4ffc9391c4aedd58d8ccb4
Federico Avalos <fedeavalos78@gmail.com>
2026-09-25 15:52:23 -0300

    Creación archivo app.js - priemros ensayos de eventListener

--- Desarrollo/js/script.js
+++ Desarrollo/js/script.js
@@ -1,1 +1,13 @@
 console.log("Script cargado correctamente.");
+
+const genPaleta = document.querySelectorAll(".gen-paleta");
+
+genPaleta.forEach(function(paleta) {
+  paleta.addEventListener('click', function(event) {
+    console.log('Paleta generada');
+    const paletaColor = `hsl(${Math.floor(Math.random() * 360)}, ${Math.floor(Math.random() * 100)}%, ${Math.floor(Math.random() * 100)}%)`;})
+
+    console.log('Paleta generada');
+
+
+});
